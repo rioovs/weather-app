d1eac2547c4e2cf4d2333ef3cb58289e80ffb594
Rio Oktaviansyah <riooktaviansyah36@gmail.com>
2024-05-02 19:07:21 +0700

    update

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const apiKey = '{APIKEY}';
+const apiKey = '{APIKEY}'; //change with apikey openweathermap
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather?units=metric&q=';
 
 const searchBox = document.querySelector(".search input");
