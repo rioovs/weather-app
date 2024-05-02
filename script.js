58519901ee411d6daf40c4c91040c91207c3c1be
Rio Oktaviansyah <riooktaviansyah36@gmail.com>
2024-05-02 19:08:44 +0700

    change to let

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ async function checkWeather(city) {
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
     }else{
-        var data = await response.json();
+        let data = await response.json();
         console.log(data);
         document.querySelector(".city").innerHTML = data.name;
         document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°C";
